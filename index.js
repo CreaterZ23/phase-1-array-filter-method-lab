8b0002dc300ceb1aa9489d614965dd365e25031e
Zeon <zeonstewart23@gmail.com>
2021-08-12 13:58:17 -0400

    commit tree 321f94c1a6871dcd253f703600f47f0d1e6a0ff9

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(driverNamesArr, nameStr) {
+
+    const matchingDriver = driverNamesArr.filter(names => names.toLowerCase() === nameStr.toLowerCase());
+    console.log(matchingDriver);
+    return matchingDriver;
+
+
+}
+
+function fuzzyMatch(driverNamesArr, nameStr) {
+    const fuzzyMatchingDriver = driverNamesArr.filter(names => names.charAt() === nameStr.charAt());
+    return fuzzyMatchingDriver;
+}
+
+function matchName(driverObjArr, driverStr) {
+    const matchingDriver = driverObjArr.filter(driverObj => driverObj.name === driverStr)
+    return matchingDriver;
+
+}
